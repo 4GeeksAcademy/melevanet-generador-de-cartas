b489e3b46a495e39269c6c8c936d847f9d34f7de
melevanet <157515892+melevanet@users.noreply.github.com>
2024-07-14 17:10:19 +0000

    cartasaleatorias

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  generateRandomCard();
 };
+
+function generateRandomCard() {
+  const suits = ["♥", "♠", "♣", "♦"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  const cardTop = document.getElementById("card-top");
+  const cardMiddle = document.getElementById("card-middle");
+  const cardBottom = document.getElementById("card-bottom");
+
+  cardTop.innerHTML = randomSuit;
+  cardMiddle.innerHTML = randomValue;
+  cardBottom.innerHTML = randomSuit;
+
+  const isRedSuit = randomSuit === "♥" || randomSuit === "♦";
+  cardTop.style.color = isRedSuit ? "red" : "black";
+  cardMiddle.style.color = isRedSuit ? "red" : "black";
+  cardBottom.style.color = isRedSuit ? "red" : "black";
+}
